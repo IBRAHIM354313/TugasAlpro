36978f442bd54e6bcc0ddf0aea67a4fabbc3eb72
IBRAHIM354313 <154485892+IBRAHIM354313@users.noreply.github.com>
2024-01-19 19:55:43 +0700

    Add files via upload

---
+++ Tugas5Alpro_ Muhammad_Musa_Ibrahim.cxx
@@ -0,0 +1,30 @@
+	#include <iostream>
+	
+	using namespace std;
+				
+				
+	long long faktorial(int n){
+		if (n == 0 || n == 1){
+			cout << n;
+		    return 1;
+    	}
+		else
+		{
+	   	cout << " " << n << " x ";
+		   return n*faktorial(n-1);                                  }
+  	}    
+								
+  	int main(){
+	 	int angka;
+	 	long long hasil;
+	 	cout <<"\t bilangan faktorial \n\n";
+	 	cout <<"masukkan angka :";        cin>>angka;
+	 	
+	hasil =faktorial(angka);
+	 cout <<"\n\n hasil faktorialnya :"<<hasil;
+
+								
+																
+  return 0;
+  }								
+										
